9f739d4c0cc00b855f013acf9bd5e8f40f7f87db
Maksim Izmaylov <me@kvakes.com>
2014-10-10 18:10:26 -0700

    If Amadeus API request fails (no flight results), then show next random entry

--- js/app.js
+++ js/app.js
@@ -41,52 +41,79 @@
         return distance > 10;
       });
 
-    showEntry(fromIATA, entries[Math.floor(Math.random() * filteredEntries.length)]);
+    showEntry(fromIATA, filteredEntries);
     $('#shuffle').on('click', function() {
-      showEntry(fromIATA, entries[Math.floor(Math.random() * filteredEntries.length)]);
+      showEntry(fromIATA, filteredEntries);
     });
   });
 
-  function showEntry(fromIATA, destination) {
-    var photoURL = destination.gsx$photourl.$t;
-
-    var imageLoaded = $.Deferred(),
-      infoLoaded = $.Deferred();
-
-    // Preload and then show the image
-    $('#preloader')
-      .attr('src', photoURL)
-      .on('load', imageLoaded.resolve);
+  function showEntry(fromIATA, filteredEntries) {
+    var shuffledEntries = shuffle(filteredEntries);
 
     var now = moment(),
       in2weeks = now.clone().add(2, 'weeks'),
       in6weeks = now.clone().add(6, 'weeks');
 
-    var toIATA = destination.gsx$iatacode.$t;
+    function tryLoadRandomEntry() {
+      var destination = shuffledEntries.pop();
 
-    $.getJSON('http://api.sandbox.amadeus.com/v1.2/flights/extensive-search?origin=' + fromIATA + '&destination=' + toIATA + '&departure_date=' + in2weeks.format('YYYY-MM-DD') + '--' + in6weeks.format('YYYY-MM-DD') + '&duration=6--14&apikey=' + AMADEUS_API_KEY)
-      .then(infoLoaded.resolve);
+      var photoURL = destination.gsx$photourl.$t;
 
-    $.when(imageLoaded.promise(), infoLoaded.promise())
-      .then(function(a, flights) {
-        $('#scene').removeClass('transition');
-        $('.loading').hide();
+      var imageLoaded = $.Deferred(),
+        infoLoaded = $.Deferred();
 
-        $('#scene')
-          .css('backgroundImage', 'url(' + photoURL + ')')
-          .addClass('transition');
+      // Preload and then show the image
+      $('#preloader')
+        .attr('src', photoURL)
+        .on('load', imageLoaded.resolve);
 
-        var bestMatch = flights[0].results[0],
-          departure_date = moment(bestMatch.departure_date, 'YYYY-MM-DD'),
-          return_date = moment(bestMatch.return_date, 'YYYY-MM-DD'),
-          price = bestMatch.price;
+      var toIATA = destination.gsx$iatacode.$t;
 
-        $('#departure-date').text(departure_date.format('MMM DD'));
-        $('#return-date').text(return_date.format('MMM DD'));
-        $('#price').text(price);
-        $('#destination').text(destination.gsx$iatacode.$t);
-        $('.book a').attr('href', 'http://www.skyscanner.com/transport/flights/' + fromIATA + '/' + toIATA + '/' + departure_date.format('YYMMDD') + '/' + return_date.format('YYMMDD'));
-      });
+      $.getJSON('http://api.sandbox.amadeus.com/v1.2/flights/extensive-search?origin=' + fromIATA + '&destination=' + toIATA + '&departure_date=' + in2weeks.format('YYYY-MM-DD') + '--' + in6weeks.format('YYYY-MM-DD') + '&duration=6--14&apikey=' + AMADEUS_API_KEY)
+        .then(infoLoaded.resolve)
+        .fail(tryLoadRandomEntry);
+
+      $.when(imageLoaded.promise(), infoLoaded.promise())
+        .then(function(a, flights) {
+          $('#scene').removeClass('transition');
+          $('.loading').hide();
+
+          $('#scene')
+            .css('backgroundImage', 'url(' + photoURL + ')')
+            .addClass('transition');
+
+          var bestMatch = flights[0].results[0],
+            departure_date = moment(bestMatch.departure_date, 'YYYY-MM-DD'),
+            return_date = moment(bestMatch.return_date, 'YYYY-MM-DD'),
+            price = bestMatch.price;
+
+          $('#departure-date').text(departure_date.format('MMM DD'));
+          $('#return-date').text(return_date.format('MMM DD'));
+          $('#price').text(price);
+          $('#destination').text(destination.gsx$iatacode.$t);
+          $('.book a').attr('href', 'http://www.skyscanner.com/transport/flights/' + fromIATA + '/' + toIATA + '/' + departure_date.format('YYMMDD') + '/' + return_date.format('YYMMDD'));
+        });
+    }
+    tryLoadRandomEntry();
   };
 
 })();
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
